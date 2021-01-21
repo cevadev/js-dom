217a494d9668af381dd282f1931ffc8c0aac0d6f
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2021-01-20 23:43:39 -0300

    Trabajando con el DOM con JS

---
+++ clases/agregar-nodos/index.js
@@ -0,0 +1,38 @@
+//creamos un elemento h1 - sin agregarlo aun al dom
+const etiquetah3 = document.createElement("h3");
+
+//creamos un texto - sin agregarlo aun al dom
+const texto = document.createTextNode("¡Hola, mundo!");
+
+//obtenemos el elemento padre
+const parentElement = document.querySelector("div");
+
+//insertamos el texto al elemento
+etiquetah3.appendChild(texto);
+
+//insertamos el nodo al elemento padre
+parentElement.appendChild(etiquetah3);
+
+//***************************************************** */
+/***
+ * Manejo del metodo append
+ */
+
+//agregamos un elemento al root
+parentElement.append("Este es un nuevo texto", document.createElement("p"));
+
+//******************************************************** */
+
+//***************************************** */
+/**
+ * utilizamos el metodo insertBefore()
+ */
+const titulo = document.createElement("h1");
+const tituloText = document.createTextNode("Trabajando con nodos");
+titulo.appendChild(tituloText);
+
+//obtenemos la referencia del elemento que quiero insertar antes
+const referencia = document.querySelector("h3");
+
+//insertamos el titulo antes de la referencia
+parentElement.insertBefore(titulo, referencia);
